110f21c6bea7c64fdfd3ed5872a9fa19751e5ed8
Raiford Brookshire <formulacoder@outlook.com>
2023-01-20 16:55:28 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace PocDataProtection
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			UseDataProtectionNative();
+			UseDataProtectionWrapper();
+
+			Console.WriteLine("Hit any key..."); 
+			Console.ReadLine();
+
+		}
+
+		private static void UseDataProtectionWrapper()
+		{
+			Console.WriteLine($"");
+			Console.WriteLine($"Using DataProtection with a Wrapper class");
+			Console.WriteLine($"-----------------------------------");
+			
+			const string appName = "My Application Name";
+			const string protectorName = "My Protector";
+			const string plainText = "My Secret in Plain Text";
+			
+			var protector = new ProtectedString(appName, protectorName);
+			
+			Console.WriteLine($"Encrypting the plain text: {plainText}");
+			var cipherText = protector.Protect(plainText);
+			Console.WriteLine($"Encrypted to Cipher Text: {cipherText}");
+
+			// Decrypt the variable
+			string secret = protector.Unprotect();
+			Console.WriteLine($"Complete: plainText={plainText} decryptedText: {secret}");
+		}
+
+		private static void UseDataProtectionNative()
+		{
+			Console.WriteLine($"");
+			Console.WriteLine($"Using DataProtection Native .NET");
+			Console.WriteLine($"-----------------------------------");
+
+
+			const string appName = "My Application Name";
+			const string plainText = "My Secret in Plain Text";
+			const string protectorName = "My Protector";
+
+			var provider = DataProtectionProvider.Create(appName);
+			var protector = provider.CreateProtector(protectorName);
+		
+			Console.WriteLine($"Encrypting the plain text: {plainText}");
+			var cipherText = protector.Protect(plainText);
+			Console.WriteLine($"Encrypted to Cipher Text: {cipherText}");
+
+			// Decrypt the variable
+			string secret = protector.Unprotect(cipherText);
+			Console.WriteLine($"Complete: plainText={plainText} decryptedText: {secret}");
+		}
+	}
+}
---
+++ ProtectedString.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+
+namespace PocDataProtection
+{
+	public class ProtectedString
+	{
+		private string _cipherText;
+		private string _appName = "My Application Name";
+		private string _protectorName = "My Protector";
+		private IDataProtectionProvider _provider;
+		private IDataProtector _protector;
+
+		public ProtectedString(string appName, string protectorName)
+		{
+			_appName = appName;
+			_protectorName = protectorName;
+			_provider = DataProtectionProvider.Create(_appName);
+			_protector = _provider.CreateProtector(protectorName);
+		}
+
+		public string Protect(string plainText)
+		{
+			return _cipherText = _protector.Protect(plainText);		
+		}
+		public string Unprotect()
+		{
+			return _protector.Unprotect(_cipherText);
+		}
+	}
+}
