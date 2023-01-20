6d549f42b80feb136df8a56845bb35bb940f7da5
Raiford Brookshire <formulacoder@outlook.com>
2023-01-20 17:30:12 -0600

    Added Wipe Memory

--- Program.cs
+++ Program.cs
@@ -9,37 +9,43 @@ namespace PocDataProtection
 			UseDataProtectionNative();
 			UseDataProtectionWrapper();
 
-			Console.WriteLine("Hit any key..."); 
+			WriteLine("Hit any key..."); 
 			Console.ReadLine();
 
 		}
 
 		private static void UseDataProtectionWrapper()
 		{
-			Console.WriteLine($"");
-			Console.WriteLine($"Using DataProtection with a Wrapper class");
-			Console.WriteLine($"-----------------------------------");
+			WriteLine($"");
+			WriteLine($"Using DataProtection with a Wrapper class");
+			WriteLine($"-----------------------------------");
 			
 			const string appName = "My Application Name";
 			const string protectorName = "My Protector";
-			const string plainText = "My Secret in Plain Text";
-			
+			string plainText = "My Secret in Plain Text";
+			string originalText = plainText;
+
 			var protector = new ProtectedString(appName, protectorName);
 			
-			Console.WriteLine($"Encrypting the plain text: {plainText}");
-			var cipherText = protector.Protect(plainText);
-			Console.WriteLine($"Encrypted to Cipher Text: {cipherText}");
+			WriteLine($"Encrypting the plain text: {plainText}");
+			var cipherText = protector.Protect(ref plainText);
+			WriteLine($"Encrypted to Cipher Text: {cipherText}");
+			WriteLine($"Original Plain Text Memory is Wiped: {plainText}");
 
 			// Decrypt the variable
 			string secret = protector.Unprotect();
-			Console.WriteLine($"Complete: plainText={plainText} decryptedText: {secret}");
+			WriteLine($"Complete: plainText (wiped)={plainText} originalText={originalText} decryptedText: {secret}");
+
+			// Finally Wipe the memory of the secret once your done using it.
+			protector.WipeStringMemory(ref secret);
+			WriteLine($"Secret has been wiped 'secret': {secret} (this should be empty)");
 		}
 
 		private static void UseDataProtectionNative()
 		{
-			Console.WriteLine($"");
-			Console.WriteLine($"Using DataProtection Native .NET");
-			Console.WriteLine($"-----------------------------------");
+			WriteLine($"");
+			WriteLine($"Using DataProtection Native .NET");
+			WriteLine($"-----------------------------------");
 
 
 			const string appName = "My Application Name";
@@ -49,13 +55,18 @@ namespace PocDataProtection
 			var provider = DataProtectionProvider.Create(appName);
 			var protector = provider.CreateProtector(protectorName);
 		
-			Console.WriteLine($"Encrypting the plain text: {plainText}");
+			WriteLine($"Encrypting the plain text: {plainText}");
 			var cipherText = protector.Protect(plainText);
-			Console.WriteLine($"Encrypted to Cipher Text: {cipherText}");
+			WriteLine($"Encrypted to Cipher Text: {cipherText}");
 
 			// Decrypt the variable
 			string secret = protector.Unprotect(cipherText);
-			Console.WriteLine($"Complete: plainText={plainText} decryptedText: {secret}");
+			WriteLine($"Complete: plainText={plainText} decryptedText: {secret}");
+		}
+
+		public static void WriteLine(string line)
+		{
+			Console.WriteLine(line);
 		}
 	}
 }
--- ProtectedString.cs
+++ ProtectedString.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.DataProtection;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace PocDataProtection
 {
 	public class ProtectedString
 	{
-		private string _cipherText;
-		private string _appName = "My Application Name";
-		private string _protectorName = "My Protector";
+		private string _cipherText = string.Empty;
+		private string _appName = "Default App Name";
+		private string _protectorName = "Default Protector Name";
 		private IDataProtectionProvider _provider;
 		private IDataProtector _protector;
 
@@ -18,13 +20,49 @@ namespace PocDataProtection
 			_protector = _provider.CreateProtector(protectorName);
 		}
 
-		public string Protect(string plainText)
+		/// <summary>
+		/// Takes a plain Text variable by ref. After the plain text is protected the "plainText" variable
+		/// will have its memory wiped for security. Keep this in mind to ensure you can no longer use
+		/// this variable and its contents are wiped from memory.
+		/// </summary>
+		/// <param name="plainText"></param>
+		/// <returns></returns>
+		public string Protect(ref string plainText)
 		{
-			return _cipherText = _protector.Protect(plainText);		
+			_cipherText = _protector.Protect(plainText);
+			WipeStringMemory(ref plainText);
+			return _cipherText;
 		}
 		public string Unprotect()
 		{
 			return _protector.Unprotect(_cipherText);
+		}
+
+		/// <summary>
+		/// Wipes the Memory of the input using Random Bytes and sets string to empty.
+		/// 
+		/// This is main for example and is likely a better with using direct memory access
+		/// in unsafe mode or perhaps a Span<byte>.
+		/// </summary>
+		/// <param name="input"></param>
+		public void WipeStringMemory(ref string input)
+		{
+			try
+			{
+				byte[] randomBytes = new byte[input.Length * sizeof(char)];
+				new Random().NextBytes(randomBytes);
+				char[] charArray = new char[input.Length];
+				Buffer.BlockCopy(randomBytes, 0, charArray, 0, randomBytes.Length);
+				input = new string(charArray);
+			}
+			finally
+			{
+				input = string.Empty;
+			}
+		
+			// This could be removed if performance is higher priority over security
+			GC.Collect();
+			GC.WaitForPendingFinalizers();
 		}
 	}
 }
